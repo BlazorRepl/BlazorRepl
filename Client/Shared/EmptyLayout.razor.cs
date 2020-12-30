7b506cdb7b834436674481c576b7ca1bb0191764
Vladislav Karamfilov <vladislav.karamfilov@gmail.com>
2020-12-30 22:28:29 +0200

    EmptyLayout code-behind file.

---
+++ Client/Shared/EmptyLayout.razor.cs
@@ -0,0 +1,50 @@
+﻿namespace BlazorRepl.Client.Shared
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.IO;
+    using System.Runtime.Loader;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Components;
+    using Microsoft.JSInterop;
+
+    public partial class EmptyLayout
+    {
+        [Inject]
+        public IJSRuntime JsRuntime { get; set; }
+
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+
+        private bool RenderBody { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            var sessionId = new Uri(this.NavigationManager.Uri).Fragment.Trim('#');
+            if (!string.IsNullOrWhiteSpace(sessionId))
+            {
+                // TODO: Extract to a service
+                // TODO: Rename JS method
+                var dlls = await this.JsRuntime.InvokeAsync<IEnumerable<string>>("App.getAvailableFiles", sessionId);
+
+                var sw = new Stopwatch();
+
+                foreach (var dll in dlls)
+                {
+                    sw.Restart();
+                    var dllBytes = Convert.FromBase64String(dll);
+                    Console.WriteLine($"string to byte[] DLL - {sw.Elapsed}");
+
+                    sw.Restart();
+                    AssemblyLoadContext.Default.LoadFromStream(new MemoryStream(dllBytes));
+                    Console.WriteLine($"loading DLL - {sw.Elapsed}");
+                }
+            }
+
+            this.RenderBody = true;
+
+            await base.OnInitializedAsync();
+        }
+    }
+}
