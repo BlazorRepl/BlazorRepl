09bd0e1b12454bed4971016dcf0b65eb8bcd2bbc
vladislav-karamfilov <vladislav.karamfilov@gmail.com>
2021-02-07 20:51:13 +0200

    Minor perf optimizations in CompilationService

--- Core/CompilationService.cs
+++ Core/CompilationService.cs
@@ -33,16 +33,12 @@
 @using Microsoft.AspNetCore.Components.Web
 @using Microsoft.JSInterop";
 
+        private static readonly CSharpParseOptions CSharpParseOptions = new(LanguageVersion.Preview);
+        private static readonly RazorProjectFileSystem RazorProjectFileSystem = new VirtualRazorProjectFileSystem();
+
         // Creating the initial compilation + reading references is on the order of 250ms without caching
         // so making sure it doesn't happen for each run.
         private static CSharpCompilation baseCompilation;
-        private static CSharpParseOptions cSharpParseOptions;
-
-        private readonly RazorProjectFileSystem fileSystem = new VirtualRazorProjectFileSystem();
-        private readonly RazorConfiguration configuration = RazorConfiguration.Create(
-            RazorLanguageVersion.Latest,
-            configurationName: "Blazor",
-            extensions: Array.Empty<RazorExtension>());
 
         public static IEnumerable<AssemblyIdentity> BaseAssemblyNames => baseCompilation.ReferencedAssemblyNames;
 
@@ -82,9 +78,8 @@
 
             baseCompilation = CSharpCompilation.Create(
                 "BlazorRepl.UserComponents",
-                Array.Empty<SyntaxTree>(),
-                basicReferenceAssemblies,
-                new CSharpCompilationOptions(
+                references: basicReferenceAssemblies,
+                options: new CSharpCompilationOptions(
                     OutputKind.DynamicallyLinkedLibrary,
                     optimizationLevel: OptimizationLevel.Release,
                     concurrentBuild: false,
@@ -94,8 +89,6 @@
                         new KeyValuePair<string, ReportDiagnostic>("CS1701", ReportDiagnostic.Suppress),
                         new KeyValuePair<string, ReportDiagnostic>("CS1702", ReportDiagnostic.Suppress),
                     }));
-
-            cSharpParseOptions = new CSharpParseOptions(LanguageVersion.Preview);
         }
 
         // TODO: think about removal of packages
@@ -157,7 +150,7 @@
             for (var i = 0; i < cSharpResults.Count; i++)
             {
                 var cSharpResult = cSharpResults[i];
-                syntaxTrees[i] = CSharpSyntaxTree.ParseText(cSharpResult.Code, cSharpParseOptions, cSharpResult.FilePath);
+                syntaxTrees[i] = CSharpSyntaxTree.ParseText(cSharpResult.Code, CSharpParseOptions, cSharpResult.FilePath);
             }
 
             var finalCompilation = baseCompilation.AddSyntaxTrees(syntaxTrees);
@@ -206,6 +199,19 @@
                 Encoding.UTF8.GetBytes(fileContent.TrimStart()));
         }
 
+        private static RazorProjectEngine CreateRazorProjectEngine(IReadOnlyList<MetadataReference> references) =>
+            RazorProjectEngine.Create(RazorConfiguration.Default, RazorProjectFileSystem, b =>
+            {
+                b.SetRootNamespace(DefaultRootNamespace);
+                b.AddDefaultImports(DefaultImports);
+
+                // Features that use Roslyn are mandatory for components
+                CompilerFeatures.Register(b);
+
+                b.Features.Add(new CompilationTagHelperFeature());
+                b.Features.Add(new DefaultMetadataReferenceFeature { References = references });
+            });
+
         private async Task<IReadOnlyList<CompileToCSharpResult>> CompileToCSharpAsync(
             ICollection<CodeFile> codeFiles,
             Func<string, Task> updateStatusFunc)
@@ -213,7 +219,7 @@
             await (updateStatusFunc?.Invoke("Preparing Project") ?? Task.CompletedTask);
 
             // The first phase won't include any metadata references for component discovery. This mirrors what the build does.
-            var projectEngine = this.CreateRazorProjectEngine(Array.Empty<MetadataReference>());
+            var projectEngine = CreateRazorProjectEngine(Array.Empty<MetadataReference>());
 
             // Result of generating declarations
             var declarations = new CompileToCSharpResult[codeFiles.Count];
@@ -259,7 +265,7 @@
 
             // Add the 'temp' compilation as a metadata reference
             var references = new List<MetadataReference>(baseCompilation.References) { tempAssembly.Compilation.ToMetadataReference() };
-            projectEngine = this.CreateRazorProjectEngine(references);
+            projectEngine = CreateRazorProjectEngine(references);
 
             var results = new CompileToCSharpResult[declarations.Length];
             for (index = 0; index < declarations.Length; index++)
@@ -288,18 +294,5 @@
 
             return results;
         }
-
-        private RazorProjectEngine CreateRazorProjectEngine(IReadOnlyList<MetadataReference> references) =>
-            RazorProjectEngine.Create(this.configuration, this.fileSystem, b =>
-            {
-                b.SetRootNamespace(DefaultRootNamespace);
-                b.AddDefaultImports(DefaultImports);
-
-                // Features that use Roslyn are mandatory for components
-                CompilerFeatures.Register(b);
-
-                b.Features.Add(new CompilationTagHelperFeature());
-                b.Features.Add(new DefaultMetadataReferenceFeature { References = references });
-            });
     }
 }
